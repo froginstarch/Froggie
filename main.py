fc4e06af198ad8982103051d69d4cb6fb3d9e168
Froginstarch <161838470+froginstarch@users.noreply.github.com>
2025-05-12 13:58:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_member_join(member):
-    await message.channel.send(f'Hello, {member}!')
+await bot.add_cog(Greetings(bot))
+
 
 client.run(os.getenv('DISCORD_API_KEY'))
